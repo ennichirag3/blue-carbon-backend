cbff2f51bded87b785143f00c72126fb9a5be600
Enni chirag <ennichirag@Ennis-MacBook-Air.local>
2025-09-15 16:26:51 +0530

    Deleted unwanted file

--- ghf.java
+++
@@ -1,5 +0,0 @@
-class ghf {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- hhgao.java
+++
@@ -1,4 +0,0 @@
-public class hhgao {
-    public static void main(String[] args) {
-        System.out.println("Hello jh World");
-}
